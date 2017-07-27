0b1fda5f9097feba1ac0bb0ce036ebb86119e5f5
yanchhuong <yanchhuo_ng@yahoo.com>
2017-07-27 14:41:14 +0700

    test

--- src/main/java/com/code/demo/HomeController.java
+++ src/main/java/com/code/demo/HomeController.java
@@ -29,14 +29,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
     private RecordRepository repository;
-
     @Autowired
     public HomeController(RecordRepository repository) {
         this.repository = repository;
     }
-
     @RequestMapping(method = RequestMethod.GET)
     public String home(ModelMap model) {
         List<Record> records = repository.findAll();
