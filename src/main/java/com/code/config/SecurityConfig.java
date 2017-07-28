de79e8ec4316c2c1a9a9f64ee34ec242a0cec60f
yanchhuong <yanchhuo_ng@yahoo.com>
2017-07-28 16:00:15 +0700

    test

--- src/main/java/com/code/config/SecurityConfig.java
+++ src/main/java/com/code/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				
 			.and()
 				.exceptionHandling().accessDeniedPage("/403")
-			.and().csrf().disable();
+			.and().csrf();
 		// add iframe work
 		http.headers().frameOptions().disable();
 	}
