363dae0b0b684ff5b10d7df927fe096c4aca97fd
yanchhuong <yanchhuo_ng@yahoo.com>
2017-07-28 13:12:06 +0700

    java

--- src/main/java/com/code/config/Record.java
+++ src/main/java/com/code/config/Record.java
@@ -1,4 +1,4 @@
-package com.heroku.demo;
+package com.code.config;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/code/config/RecordRepository.java
+++ src/main/java/com/code/config/RecordRepository.java
@@ -1,4 +1,4 @@
-package com.heroku.demo;
+package com.code.config;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
