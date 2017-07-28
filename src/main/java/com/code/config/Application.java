872d0fc04145a478e92d7081af6b65f7066f3494
yanchhuong <yanchhuo_ng@yahoo.com>
2017-07-28 13:35:13 +0700

    config

--- src/main/java/com/code/config/Application.java
+++ src/main/java/com/code/config/Application.java
@@ -21,14 +21,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages="com.code")
 @SpringBootApplication
+
+@EntityScan(basePackages = {"com.code.*"})
+@EnableJpaRepositories(basePackages = "com.code.*")
 public class Application extends SpringBootServletInitializer {
 private static  Logger LOGGER =  LoggerFactory.getLogger(Application.class);
 	
