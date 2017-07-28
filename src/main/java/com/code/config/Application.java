6036e990ceff275ccfb05ebf295bc59fefe0842c
yanchhuong <yanchhuo_ng@yahoo.com>
2017-07-28 13:24:02 +0700

    conf

--- src/main/java/com/code/config/Application.java
+++ src/main/java/com/code/config/Application.java
@@ -15,14 +15,30 @@
  */
 package com.code.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+private static  Logger LOGGER =  LoggerFactory.getLogger(Application.class);
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+      //  SpringApplication.run(Application.class, args);
+    	 LOGGER.info("Start to Access URLs:HEROKU.......");
+    	 
+        SpringApplication app = new SpringApplication(Application.class);
+      //  app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);   
+        LOGGER.info("Finish Access URLs:HEROKU");
     }
-
 }
